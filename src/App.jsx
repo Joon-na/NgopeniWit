ad95edd7e235f174e9f9ad67dbede93cb9b2da1b
Arjun Nafi <arjunnafi04@gmail.com>
2024-11-19 00:02:32 +0700

    feat: login

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
-import BudidayaTracker from './pages/BudidayaTracker'
-import Statistics from './pages/Statistics'
+import BudidayaTracker from './pages/BudidayaTracker';
+import Statistics from './pages/Statistics';
 import InfoBudidaya from './pages/InfoBudidaya';
 import About from './pages/About';
+import Login from './pages/Login';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
+  const [user, setUser ] = useState(null);
+
+  useEffect(() => {
+    // Memuat user dari localStorage saat aplikasi dimuat
+    const storedUser  = localStorage.getItem('user');
+    if (storedUser ) {
+      setUser (JSON.parse(storedUser ));
+    }
+  }, []);
+
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="budidaya-tracker" element={<BudidayaTracker />} />
+          <Route path="login" element={<Login setUser ={setUser } />} />
+          <Route 
+            path="budidaya-tracker" 
+            element={<PrivateRoute element={<BudidayaTracker user={user} />} user={user} />} 
+          />
           <Route path="info-budidaya" element={<InfoBudidaya />} />
           <Route path="statistics" element={<Statistics />} />
         </Route>
